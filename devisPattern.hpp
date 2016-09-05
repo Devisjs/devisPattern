2e223a03d060702e21f3169faf9029387fc37a2d
Ismail Reida <ismailrei@gmail.com>
2016-09-05 16:32:21 +0100

    Update devisPattern.hpp

--- devisPattern.hpp
+++ devisPattern.hpp
@@ -34,7 +34,7 @@ namespace devisPattern {
         static void find(const v8::FunctionCallbackInfo<v8::Value>& args);
         static void add(const v8::FunctionCallbackInfo<v8::Value>& args);
         static v8::Persistent<v8::Function> constructor;
-        static TStrStrMap tMap;
+         TStrStrMap tMap;
 
     };
     
