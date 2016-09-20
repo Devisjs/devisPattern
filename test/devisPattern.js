acfd93166e6bbb047bfbb51032159d7f4bbdfc97
ismailrei <ismailrei@gmail.com>
2016-09-20 11:30:57 +0100

    add test

---
+++ test/devisPattern.js
@@ -0,0 +1,39 @@
+const devisPattern = require("../devisPattern");
+const date = require("./time");
+
+let i;
+let LIMIT = 2900;
+for (i = 0; i < LIMIT; i++) {
+    date.time(i);
+    devisPattern.add({
+        id: i,
+        cmd: i
+    }, (args, done) => {
+
+        let x = args.ident;
+        let somme = args.data1 + args.data2 + args.data3 + args.data4;
+        done(somme);
+    });
+}
+
+for (j = 0; j < LIMIT; j++) {
+    let i = Math.trunc(Math.random() * 100);
+
+    devisPattern.call({
+        id: i,
+        cmd: i
+    }, {
+        ident: i,
+        data1: 2 * i,
+        data2: 3 * i,
+        data3: 4 * i,
+        data4: 5 * i
+    }, (res) => {
+        console.log(res);
+        date.timeEnd(i);
+
+    });
+}
+
+console.log(date.moyenne()/1000+"s");
+//console.log(date.min()/1000+"s");
---
+++ test/time.js
@@ -0,0 +1,38 @@
+class date{
+  constructor()
+  {
+    this.times=[];
+    this.end=[];
+  }
+  time(label)
+  {
+    this.times[label]=Date.now();
+  }
+  timeEnd(label)
+  {
+    let time = this.times[label];
+    if (!time) {
+      throw new Error('No such label: ' + label);
+    }
+    var duration = Date.now() - time;
+    this.end[label]=duration;
+  }
+  max()
+  {
+    return Math.max(...this.end);
+  }
+  min()
+  {
+    return Math.min(...this.end);
+  }
+  moyenne()
+  {
+    let sum=0;
+    for(let i=0;i<this.end.length;i++)
+    {
+      sum+=this.end[i];
+    }
+    return sum/this.end.length;
+  }
+}
+module.exports=new date();
